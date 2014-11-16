b97f4a29b4ce8940139e425f287c0e41c2263270
Pawel Piotrowicz <ppiotrowicz@gmail.com>
2014-11-16 16:31:01 +0100

    Statistics and single test runs

--- src/TestLogger.cs
+++ src/TestLogger.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
 
@@ -23,11 +24,39 @@ namespace AwesomeTestLogger
         private void OnTestRunComplete(object sender, TestRunCompleteEventArgs e)
         {
             // complete test run
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("All: {0}",  e.TestRunStatistics.ExecutedTests);
+            Console.WriteLine("Passed:  {0}", e.TestRunStatistics[TestOutcome.Passed]);
+            Console.WriteLine("Failed:  {0}", e.TestRunStatistics[TestOutcome.Failed]);
+            Console.WriteLine();
+            Console.WriteLine("Total time: {0}", e.ElapsedTimeInRunningTests);
         }
 
         private void OnTestResult(object sender, TestResultEventArgs e)
         {
-            // single test result
+            var result = e.Result;
+
+            switch (result.Outcome)
+            {
+                case TestOutcome.None:
+                    break;
+                case TestOutcome.Passed:
+                    Console.Write(".");
+                    break;
+                case TestOutcome.Failed:
+                    Console.Write("F");
+                    break;
+                case TestOutcome.Skipped:
+                    Console.WriteLine("S");
+                    break;
+                case TestOutcome.NotFound:
+                    Console.WriteLine("?");
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
     }
 }
