fbed0c6b539a09c28ba3e466b02127d7e54a076f
Pawel Piotrowicz <ppiotrowicz@gmail.com>
2014-11-16 15:46:38 +0100

    Initial commit

---
+++ src/TestLogger.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+
+namespace AwesomeTestLogger
+{
+    [ExtensionUri("logger://Microsoft/TestPlatform/AwesomeLogger/v1")]
+    [FriendlyName("AwesomeLogger")]
+    public class TestLogger : ITestLogger
+    {
+        public void Initialize(TestLoggerEvents events, string testRunDirectory)
+        {
+            events.TestRunMessage += OnTestRunMessage;
+            events.TestRunComplete += OnTestRunComplete;
+            events.TestResult += OnTestResult;
+        }
+
+        private void OnTestRunMessage(object sender, TestRunMessageEventArgs e)
+        {
+            // ?
+        }
+
+        private void OnTestRunComplete(object sender, TestRunCompleteEventArgs e)
+        {
+            // complete test run
+        }
+
+        private void OnTestResult(object sender, TestResultEventArgs e)
+        {
+            // single test result
+        }
+    }
+}
